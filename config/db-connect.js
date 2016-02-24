d1502b36f42085e5f3318ad374eaddbb8677fa85
Nicholas Major <nick@nmajor.com>
2016-02-25 05:27:44 +0700

    Adding docker files

--- config/db-connect.js
+++ config/db-connect.js
@@ -4,7 +4,7 @@ module.exports = function() {
     password: process.env.DB_PASSWORD || 'spiderman',
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 27017,
-    database: process.env.DB_DATABASE || 'hobbyhub-dev'
+    database: process.env.DB_NAME || 'hobbyhub-dev'
   };
 
   var url = 'mongodb://'+dbConfig.username+':'+dbConfig.password+'@'+dbConfig.host+':'+dbConfig.port+'/'+dbConfig.database;
