a9be6d39024e4f69053257e2843039dd8fc7e2fa
Nicholas Major <nick@nmajor.com>
2016-03-01 22:49:39 +0700

    Making hobby desc not required

--- models/hobby.js
+++ models/hobby.js
@@ -23,7 +23,7 @@ var HobbySchema = new Schema({
   difficulty: Number,
   startingCost: [],
   repeatCost: [],
-  desc: { type: String, required: 'Description is required!' },
+  desc: type: String,
   resources: [{
     ref: String,
     text: String
